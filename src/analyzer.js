bbf9c64fef2d613d3a4e1972178a8d7f5120b492
Rosario <rosario.trillo@unmsm.edu.pe>
2023-09-22 18:42:35 -0500

    tres funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,16 +1,25 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    let conteopalabras = text.split(" ");
-    return conteopalabras.length;
 
+  getWordCount: (text) => {
+    let conteoPalabras = text.replace(/[^a-zA-Z\s]/g, '');
+    let palabrasSin = conteoPalabras.split(/\s+/);
+    const palabrasSinEspacio = palabrasSin.filter(function(palabra) { //No comprendo esta línea de código
+      return palabra.length > 0;
+    })
+    return palabrasSinEspacio.length; 
+    //Elimiar espacios, números y signos 
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
 
   getCharacterCount: (text) => {
-    return text.length;
+  return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+    let conteoCaracteres = text.replace(/[^0-9a-zA-Z]/g, '');
+    let conteoCaracteresSin = conteoCaracteres.replace(/\s+/g, '');
+    return conteoCaracteresSin.length;
+
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
   getAverageWordLength: (text) => {    
--- src/index.js
+++ src/index.js
@@ -3,32 +3,74 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 // Iniciar los conteos en 0
-document.querySelector(".palabras").innerHTML="Conteo de palabras:"+ 0;
+document.querySelector("li[data-testid='word-count']").textContent="Contador de palabras:" + 0;
 
-document.querySelector(".caracteres").innerHTML="Conteo de caracteres:"+ 0;
+document.querySelector("li[data-testid='character-count']").innerHTML="Conteo de caracteres:"+ 0;
 
-document.querySelector(".caracterestotales").innerHTML="Caracteres totales:"+ 0;
+document.querySelector("li[data-testid='character-no-spaces-count']").innerHTML="Conteo de caracteres sin espacio:"+ 0;
 
-document.querySelector(".numeros").innerHTML="Conteo de números:"+ 0;
+document.querySelector("li[data-testid='number-count']").innerHTML="Conteo de números:"+ 0;
 
-document.querySelector(".suma").innerHTML="Suma de números:"+ 0;
+document.querySelector("li[data-testid='number-sum']").innerHTML="Suma de números:"+ 0;
 
-document.querySelector(".longitud").innerHTML="Longitud media:"+ 0;
+document.querySelector("li[data-testid='word-length-average']").innerHTML="Longitud media:"+ 0;
 
-let conteoTexto = document.querySelector("textarea[name='user-input']");
-conteoTexto.addEventListener("input",actualizar);
+//Limpiar botón
+let botones = document.getElementById("reset-button");
+let textarea = document.querySelector("textarea[name='user-input']");
 
-function actualizar(e) {
-    document.querySelector(".caracteres").innerHTML="Conteo de caracteres:"+ analyzer.getCharacterCount(e.target.value);
-}
+botones.addEventListener("click",function() {
+        textarea.value = "Ross";
+        })
 
-let boton = document.getElementById("reset-button");
-let limpiar = document.querySelector("textarea[name='user-input']");
+// CONTEO PALABRAS
+textarea.addEventListener("input" , function(){
+    let primero = document.querySelector("li[data-testid='word-count']");      
+    let conteoPalabritas = analyzer.getWordCount("ROSARIO TRILLO");
 
-boton.addEventListener("click",function() {
-    limpiar.value = ""
-    document.querySelector(".caracteres").innerHTML="Conteo de caracteres:"+ 0;
+    primero.textContent = "Conteo de palabras: " + conteoPalabritas;
+
 })
+
+// CONTEO CARACTERES
+
+textarea.addEventListener("input" , function() {
+    let segundo = document.querySelector("li[data-testid='character-count']");
+    let conteoCaracateress = analyzer.getCharacterCount("Rosario");
+
+    segundo.textContent = "Conteo de caracteres: " + conteoCaracateress;
+})
+
+
+//CONTEO CARACTERES SIN ESPACIO
+
+textarea.addEventListener("input" , function() {
+    let tercero = document.querySelector("li[data-testid='character-no-spaces-count']");
+    let conteoSinEspacio = analyzer.getCharacterCountExcludingSpaces("Ros@rio");
+
+    tercero.textContent = "Conteo caracteres sin espacio: " + conteoSinEspacio;
+
+})
+
+
+//Limpiar botón
+//let conteoTexto = document.querySelector("textarea[name='user-input']");
+//conteoTexto.addEventListener("input",actualizar);
+
+//function actualizar(e) {
+  //  document.querySelector("li[data-testid='character-count']").innerHTML="Conteo de caracteres:"+ analyzer.getCharacterCount(e.target.value);
+//}
+// conteo de caracteres
+//let boton = document.getElementById("reset-button");
+//let limpiar = document.querySelector("textarea[name='user-input']");
+
+//boton.addEventListener("click",function() {
+  //  limpiar.value = ""
+    //document.querySelector("li[data-testid='character-count']").innerHTML="Conteo de caracteres:"+ 0;
+//})
+
+//conteo de palabras
+
 
 
 
